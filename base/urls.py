71c26a8b4cbcde72d817471fe5ebfdadf38ee926
Nathaniel81 <nathanield356@gmail.com>
2023-03-07 20:47:25 +0300

    Refactor urls code with better comments and docstrings

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,28 @@
 from django.urls import path
 from . import views
 
+"""
+This module contains all URL patterns for the chat application.
+
+The urlpatterns list maps URL patterns to their corresponding view functions.
+
+URL Patterns:
+- /login/: The login page.
+- /logout/: The logout page.
+- /register/: The registration page.
+- /: The homepage.
+- /room/<str:pk>/: The page for a specific chat room.
+- /profile/<str:pk>/: The user profile page.
+- /create-room/: The page for creating a new chat room.
+- /update-room/<str:pk>/: The page for updating an existing chat room.
+- /delete-room/<str:pk>/: The page for deleting an existing chat room.
+- /delete-message/<str:pk>/: The page for deleting a specific message.
+- /update-user/: The page for updating the current user's profile.
+- /topics/: The page listing all available chat topics.
+- /activity/: The page listing all recent chat activity.
+
+"""
+
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
