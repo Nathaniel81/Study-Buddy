32bafeaccf88347ffcbd50004472e09ec2ffe25e
Nathaniel81 <nathanield356@gmail.com>
2023-03-07 20:11:36 +0300

    Added docstrings and comments to serializer and URL patterns

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,8 +1,20 @@
+"""
+Serializes the `Room` model using Django REST Framework's `ModelSerializer`.
+
+This serializer includes all fields of the `Room` model.
+
+Functions:
+    RoomSerializer: A class-based serializer for the `Room` model.
+"""
+
 from rest_framework.serializers import ModelSerializer
 from base.models import Room
 
 
 class RoomSerializer(ModelSerializer):
+    """
+    A class-based serializer for the `Room` model.
+    """
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = '__all__'  # includes all fields in the model
