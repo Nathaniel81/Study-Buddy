c6e6b3283affeb27a89b666649191b8565397774
Nathaniel81 <nathanield356@gmail.com>
2023-03-07 20:34:15 +0300

    Add docstring to BaseConfig class

--- base/apps.py
+++ base/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class BaseConfig(AppConfig):
+    """
+    AppConfig for the 'base' app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
