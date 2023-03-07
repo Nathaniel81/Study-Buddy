8c2a129454d29d97ebadbecff542ff0620a772be
Nathaniel81 <nathanield356@gmail.com>
2023-03-07 20:15:37 +0300

    Added comments to specify view functions for URL patterns

--- base/api/views.py
+++ base/api/views.py
@@ -1,12 +1,25 @@
+"""
+Defines API views for the base app.
+
+This module includes view functions that handle GET requests to return JSON data for the API.
+
+Functions:
+    getRoutes: Returns a list of available API routes.
+    getRooms: Returns a list of all rooms in the database.
+    getRoom: Returns the details of a specific room with the given id.
+"""
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
 from .serializers import RoomSerializer
-from base.api import serializers
 
 
 @api_view(['GET'])
 def getRoutes(request):
+    """
+    Returns a list of available API routes.
+    """
     routes = [
         'GET /api',
         'GET /api/rooms',
@@ -17,6 +30,9 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getRooms(request):
+    """
+    Returns a list of all rooms in the database.
+    """
     rooms = Room.objects.all()
     serializer = RoomSerializer(rooms, many=True)
     return Response(serializer.data)
@@ -24,6 +40,9 @@ def getRooms(request):
 
 @api_view(['GET'])
 def getRoom(request, pk):
+    """
+    Returns the details of a specific room with the given id.
+    """
     room = Room.objects.get(id=pk)
     serializer = RoomSerializer(room, many=False)
     return Response(serializer.data)
