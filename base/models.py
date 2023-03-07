e68342ce835a07d046c2e0851156558a2876438a
Nathaniel81 <nathanield356@gmail.com>
2023-03-07 20:42:24 +0300

    Add docstrings to models in base app

--- base/models.py
+++ base/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    """
+    Custom user model with additional fields.
+    """
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
@@ -14,6 +17,9 @@ class User(AbstractUser):
 
 
 class Topic(models.Model):
+    """
+    Model representing a discussion topic.
+    """
     name = models.CharField(max_length=200)
 
     def __str__(self):
@@ -21,6 +27,9 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
+    """
+    Model representing a chat room.
+    """
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
@@ -38,6 +47,9 @@ class Room(models.Model):
 
 
 class Message(models.Model):
+    """
+    Model representing a chat message.
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
